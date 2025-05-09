51e5b036d60668e9fed172e28d6a056627a16057
Felix Miranda <felix.miranda.sandoval@gmail.com>
2025-05-08 22:39:33 -0400

    Creación de variables de entorno y list keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+
   const [criptos, setCriptos] = useState();
 
   useEffect(() => {
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1')
+    fetch(`${API_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1`)
       .then((resp) => resp.json())
       .then((data) => {
         setCriptos(data);
